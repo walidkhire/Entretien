550623e311b5ae16c7db5fb9b39b55761c5c0188
Walid <walidkhireddine88@gmail.com>
2026-01-27 19:43:01 +0100

    Ajouter une porojet qui permis de recuperer les mots qui sont proche a un terme propose

---
+++ MotProche/ISuggestionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotProche
+{
+    public interface ISuggestionService
+    {
+        List<string> GetSuggestions(string terme, List<string> liste, int N);
+    }
+}
---
+++ MotProche/SuggestionService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotProche
+{
+    public class SuggestionService : ISuggestionService
+    {
+        public List<string> GetSuggestions(string terme, List<string> liste, int N)
+        {
+            terme = terme.ToLower();
+            var Condidats = new List<(string mot, int DiffScore)>();
+            foreach (string mot in liste)
+            {
+                if (terme.Length > mot.Length)
+                    continue;
+
+                int Difference = GetDifferenceScore(terme, mot.ToLower());
+                Condidats.Add((mot, Difference));
+
+            }
+
+            return Condidats
+                  .OrderBy(t => t.DiffScore)
+                  .ThenBy(t => Math.Abs(terme.Length - t.mot.Length))
+                  .ThenBy(t => t.mot)
+                  .Take(N)
+                  .Select(t => t.mot)
+                  .ToList();
+        }
+
+        private int GetDifferenceScore(string src, string dest)
+        {
+            int differences = 0;
+            for (int i = 0; i < src.Length; i++)
+                if (src[i] != dest[i]) differences++;
+
+            return differences;
+        }
+    }
+}
