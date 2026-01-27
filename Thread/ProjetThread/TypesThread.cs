d83ebe24d672d5505f474541fadf62b2189cf234
Walid <walidkhireddine88@gmail.com>
2026-01-27 10:38:38 +0100

    Projet qui montre les différant type de Thread

---
+++ Thread/ProjetThread/TypesThread.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetThread
+{
+    public class TypesThread
+    {
+        // ============================================================
+        // Thread manuel
+        // ============================================================
+        public static void Travail()
+        {
+            Console.WriteLine($"Thread manuel - ID {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        // ============================================================
+        // ThreadPool / Timer
+        // ============================================================
+        public static void Travail(object? state)
+        {
+            Console.WriteLine($"ThreadPool / Timer - ID {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        // ============================================================
+        // async / await
+        // ============================================================
+        public static async Task TravailAsync()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Travail asynchrone terminé");
+        }
+    }
+}
